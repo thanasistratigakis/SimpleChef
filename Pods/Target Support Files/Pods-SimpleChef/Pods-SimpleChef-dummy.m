85df128c244ce70d0cfa27da93d558fc0fa4acea
Thanasi Stratigakis <thanasi.strati@gmail.com>
2016-10-15 22:05:23 -0700

    pods

---
+++ Pods/Target Support Files/Pods-SimpleChef/Pods-SimpleChef-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SimpleChef : NSObject
+@end
+@implementation PodsDummy_Pods_SimpleChef
+@end
---
+++ Pods/Target Support Files/Pods-SimpleChef/Pods-SimpleChef-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SimpleChefVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SimpleChefVersionString[];
+
